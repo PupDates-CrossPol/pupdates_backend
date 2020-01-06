5e400a40826cdd96332ec3b0bf1a40846558eeb0
Andrew Johnson <manilda7@gmail.com>
2020-01-06 14:49:34 -0700

    Adds updated seeds and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_181459) do
+ActiveRecord::Schema.define(version: 2020_01_06_204743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2019_12_19_181459) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "dog_images", "dogs"
