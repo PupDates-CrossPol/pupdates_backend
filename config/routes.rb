26fd849ea78e2c0e1b57463478469d2715af7cf3
Andrew Johnson <manilda7@gmail.com>
2020-01-05 17:47:30 -0700

    Adds all current neccessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
   namespace :api do
     namespace :v1 do
-      resources :users, only: :index do
+      get '/login', to: 'sessions#new', as: 'login'
+      post '/login/:email', to: 'sessions#create'
+      resources :users, only: [:index, :show] do
+        get "/dogs", to: 'users/dogs#index'
+      end
+      resources :dogs, only: :index do
+      end
+      resources :dog_images, only: [:index, :show] do
       end
     end
   end
