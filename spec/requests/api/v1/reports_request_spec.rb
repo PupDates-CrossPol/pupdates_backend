ac5c3bb5e9081000f3053f4ee5e9457e9503e687
Andrew Johnson <manilda7@gmail.com>
2020-01-07 11:23:19 -0700

    Adds update to all specs with new data

--- spec/requests/api/v1/reports_request_spec.rb
+++ spec/requests/api/v1/reports_request_spec.rb
@@ -2,13 +2,23 @@ require 'rails_helper'
 
 describe "Reports API" do
   before :each do
-    @user1 = User.create!(id: 1, first_name: 'Matt', last_name: 'Malone', email: 'mattmalone@email.com', password: 'password', description: 'a guy who likes dogs', image: 'https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
-    @user2 = User.create!(id: 2, first_name: 'Sam', last_name: 'Coleman', email: 'samcoleman@email.com', password: 'password', description: 'a guy who from TN likes dogs a lot', image: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
-    @user3 = User.create!(id: 3, first_name: 'Andrew', last_name: 'Johnson', email: 'andrewjohnson@email.com', password: 'password', description: 'a guy from SD who likes dogs', image: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
-    @user4 = User.create!(id: 4, first_name: 'Sara', last_name: 'Karsh', email: 'sarakarsh@email.com', password: 'password', description: 'a girl who likes her dog too much', image: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
+    @user1 = User.create!(first_name: 'Matt', last_name: 'Malone', email: 'mattmalone@email.com', password: 'password', description: 'a guy who likes dogs', image: 'https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
+    @user2 = User.create!(first_name: 'Sam', last_name: 'Coleman', email: 'samcoleman@email.com', password: 'password', description: 'a guy who from TN likes dogs a lot', image: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
+    @user3 = User.create!(first_name: 'Andrew', last_name: 'Johnson', email: 'andrewjohnson@email.com', password: 'password', description: 'a guy from SD who likes dogs', image: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
+    @user4 = User.create!(first_name: 'Sara', last_name: 'Karsh', email: 'sarakarsh@email.com', password: 'password', description: 'a girl who likes her dog too much', image: 'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
 
-    @report1 = Report.create!(id: 1, user_id: 4, description: 'does not have a dog')
-    @report2 = Report.create!(id: 2, user_id: 2, description: 'dog is mean')
+    @dog1 = Dog.create!(user_id: @user1.id, name: 'Fido', sex: 'male', breed: 'golden retriever', size: 'large', age: 4, fixed: true, vaccinated: true, good_with_kids: true)
+    @dog2 = Dog.create!(user_id: @user2.id, name: 'Hank', sex: 'male', breed: 'border collie', size: 'medium', age: 1, fixed: false, vaccinated: false, good_with_kids: true)
+    @dog3 = Dog.create!(user_id: @user2.id, name: 'Goofy', sex: 'male', breed: 'doberman', size: 'large', age: 5, fixed: true, vaccinated: true, good_with_kids: false)
+    @dog4 = Dog.create!(user_id: @user3.id, name: 'Molly', sex: 'female', breed: 'golden retriever', size: 'large', age: 2, fixed: true, vaccinated: true, good_with_kids: true)
+    @dog5 = Dog.create!(user_id: @user3.id, name: 'Sparkles', sex: 'female', breed: 'papillon', size: 'small', age: 8, fixed: true, vaccinated: true, good_with_kids: false)
+    @dog6 = Dog.create!(user_id: @user3.id, name: 'Fluffy', sex: 'female', breed: 'chihuahua', size: 'very small', age: 12, fixed: false, vaccinated: false, good_with_kids: false)
+    @dog7 = Dog.create!(user_id: @user3.id, name: 'Larry', sex: 'male', breed: 'german shepherd', size: 'large', age: 5, fixed: true, vaccinated: true, good_with_kids: true)
+    @dog8 = Dog.create!(user_id: @user4.id, name: 'Oliver', sex: 'male', breed: 'chocolate lab', size: 'large', age: 1, fixed: true, vaccinated: true, good_with_kids: true)
+    @dog9 = Dog.create!(user_id: @user4.id, name: 'Tallulah', sex: 'female', breed: 'mutt', size: 'medium', age: 6, fixed: true, vaccinated: true, good_with_kids: false)
+
+    @report1 = Report.create!(user_id: @user4.id, description: 'does not have a dog')
+    @report2 = Report.create!(user_id: @user2.id, description: 'dog is mean')
   end
 
   it "Gets a list of all reports" do
@@ -22,7 +32,7 @@ describe "Reports API" do
   end
 
   it "Creates a new report" do
-    post "/api/v1/reports?user_id=3&description=No good doggos"
+    post "/api/v1/reports?user_id=#{@user3.id}&description=No good doggos"
 
     expect(response).to be_successful
 
