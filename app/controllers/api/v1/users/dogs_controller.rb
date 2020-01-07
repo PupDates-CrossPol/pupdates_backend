1020f3c45ed72b774283a0f7db4c46506df00149
Andrew Johnson <manilda7@gmail.com>
2020-01-07 10:42:39 -0700

    Adds create action and dog_params method

--- app/controllers/api/v1/users/dogs_controller.rb
+++ app/controllers/api/v1/users/dogs_controller.rb
@@ -2,4 +2,19 @@ class Api::V1::Users::DogsController < ApplicationController
   def index
     render json: DogSerializer.new(User.find(params[:user_id]).dogs)
   end
+
+  def create
+    dog = Dog.new(dog_params)
+    if dog.save
+      render json: DogSerializer.new(User.find(params[:user_id]).dogs)
+    else
+      render json: "Doggo cannot be saved"
+    end
+  end
+
+  private
+
+  def dog_params
+    params.permit(:name, :sex, :breed, :size, :age, :fixed, :vaccinated, :good_with_kids, :user_id)
+  end
 end
