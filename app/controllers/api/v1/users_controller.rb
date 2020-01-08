6323714e96af73018f0a78dfaca1d93b445a9003
Andrew Johnson <manilda7@gmail.com>
2020-01-08 16:05:25 -0700

    Removes pry

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
   def index
     render json: UserSerializer.new(User.all)
   end
@@ -12,7 +12,6 @@ class Api::V1::UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     if current_user
-      binding.pry
       user.update(image: "#{params[:image]}&token=#{params[:token]}")
       user.reload
       render json: UserSerializer.new(User.find(params[:id]))
