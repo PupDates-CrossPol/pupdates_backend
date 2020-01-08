ccb044d7b4be1780c20e6346f3d3742c1d5dac11
Andrew Johnson <manilda7@gmail.com>
2020-01-08 09:31:42 -0700

    Adds user/matches controller

---
+++ app/controllers/api/v1/users/matches_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::Users::MatchesController < ApplicationController
+  def index
+    render json: UserSerializer.new(User.find_by(id: params[:user_id]).user_matches)
+  end
+
+  def create
+    match = Match.new(match_params)
+    if match.save
+      render json: "Success!"
+    else
+      render json: "Match cannot be saved"
+    end
+  end
+
+  private
+
+  def match_params
+    params.permit(:match_id, :status, :user_id)
+  end
+end
